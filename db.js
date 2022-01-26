c983e6713e525d79b6f46e1b009363216f2fed83
Rijo George <rijogeorge@Rijos-MacBook-Pro-2.local>
2022-01-26 15:47:26 -0800

    Step 1: Add a Database

--- db.js
+++ db.js
@@ -1,2 +1,18 @@
 /** Database setup for BizTime. */
+const { client } = require("pg");
 
+let DB_URI;
+
+if (process.env.NODE_ENV === "test") {
+  DB_URI = "postgresql:///users_test";
+} else {
+  DB_URI = "postgresql:///users";
+}
+
+let db = new client({
+  connectionString: DB_URI,
+});
+
+db.connect();
+
+exports.module = db;
