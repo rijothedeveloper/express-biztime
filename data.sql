dd3bb4fad19b69a39f160f7520b66217bec18b87
Rijo George <rijogeorge@rijos-mbp-2.lan>
2022-01-27 16:20:30 -0800

    data.sql updated for new tables

--- data.sql
+++ data.sql
@@ -19,6 +19,16 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industry (
+  code text PRIMARY KEY,
+  industry text NOT NULL UNIQUE
+);
+
+CREATE TABLE comp_industry (
+  comp_code text NOT NULL REFERENCES companies,
+  industry_code text NOT NULL REFERENCES industry
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
@@ -28,3 +38,12 @@ INSERT INTO invoices (comp_Code, amt, paid, paid_date)
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industry
+  VALUES ('acc', 'Accounting'),
+          ('eng', 'Engineering');
+
+INSERT INTO comp_industry
+ VALUES ('apple', 'acc'),
+        ('apple', 'eng'),
+        ('ibm', 'eng');
